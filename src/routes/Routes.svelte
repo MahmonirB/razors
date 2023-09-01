d90a5a60c5864de51d4d200ec95d81d9ea310dfe
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-09-01 22:14:13 +0200

    feat: Add routes

---
+++ src/routes/Routes.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Route, Router, Link } from 'svelte-routing';
+  import Home from '../pages/Home.svelte';
+  import About from '../pages/About.svelte';
+  import Login from '../pages/Login.svelte';
+  import Checkout from '../pages/Checkout.svelte';
+  import Products from '../pages/Products.svelte';
+  import ProductTemplate from '../pages/ProductTemplate.svelte';
+</script>
+
+<Router>
+    <nav class="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/products">Products</Link>
+    </nav>
+  
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/login" component={Login} />
+    <Route path="/checkout" component={Checkout} />
+    <Route path="/products" component={Products} />
+    <Route path="/products/:id" component={ProductTemplate} />
+  </Router>
