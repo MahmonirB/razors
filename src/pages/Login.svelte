73c6d960c4cd1fbadf5c534c4e3ee1825bd24787
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-09-16 21:47:17 +0200

    feat: create login form

--- src/pages/Login.svelte
+++ src/pages/Login.svelte
@@ -1,4 +1,66 @@
-<h1>This is Login Page.</h1>
+<script>
+    let email = '';
+    let password = '';
+    let username = 'default username';
+    let isMember = true;
+    $: isEmpty = !email || !password || !username;
+
+    function toggleMmeber() {}
+    async function handleSubmit() {}
+</script>
 <svelte:head>
     <title>Login</title>
 </svelte:head>
+
+<section class='form'>
+    <h2 class="section-title">
+        {#if isMember} Sign in {:else} register {/if}
+    </h2>
+    <form action="" on:submit|preventDefault={handleSubmit} class="login-form">
+        <div class="form-control">
+            <label for="email">email</label>
+            <input type="email" id="email" bind:value={email} />
+        </div>
+
+        <div class="form-control">
+            <label for="password">password</label>
+            <input type="password" id="password" bind:value={password} />
+        </div>
+
+        {#if isMember}
+          <div class="form-control">
+              <label for="username">username</label>
+              <input type="text" id="username" bind:value={username} />
+          </div>
+        {/if}
+
+        {#if isEmpty}
+        <p class="form-empty">
+            Please fill put all form fields
+        </p>
+        {/if}
+
+        <button
+        type="submit"
+        class="btn btn-block btn-primary"
+        disabled={isEmpty}
+        class:disabled={isEmpty}
+        >
+        Submit
+        </button>
+
+        {#if isEmpty}
+        <p class="register-link">
+            Need to register
+        <button type="button" on:click={toggleMmeber}>click hear</button>
+        </p>
+        {:else}
+        <p class="register-link">
+            already a member?
+        <button type="button" on:click={toggleMmeber}>click hear</button>
+        </p>
+        {/if}
+    </form>
+
+</section>
+
